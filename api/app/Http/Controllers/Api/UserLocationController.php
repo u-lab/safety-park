9c97183f67ab3b95e9ca52fb28ff593385836df3
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-07-02 21:28:45 +0900

    api.phpへの追記とページネーションの準備

--- api/app/Http/Controllers/Api/UserLocationController.php
+++ api/app/Http/Controllers/Api/UserLocationController.php
@@ -11,6 +11,28 @@ class UserLocationController extends Controller
 {
     public function index(Request $request)
     {
-        
+        $token = $request->header('X-API-TOKEN'); //tokenを取ってくる
+        $user = User::where('token', '=', $token)->first(); //userの照合　データベースから一致するもの取り出す
+        $user_location = UserLocation::where('user_id', '=', $user->id)->paginate(10);;//10件だけ取得する
+        return $user_location;
+    }
+
+    public function create(Request $request)
+    {
+        $token = $request->header('X-API-TOKEN');
+
+        $park_id = $request->parkID;//requestから来た配列から取り出していく
+        $longitude = $request->longitude;
+        $latitude = $request->latitude;
+
+        $user = User::where('token', '=', $token)->first();//userの照合　データベースから一致するもの取り出す
+
+        //データベースをここで作っている？
+        $user_location = UserLocation::create([
+            'user_id' => $user->id,
+            'park_id' => $park_id,
+            'longitude' => $longitude,
+            'latitude' => $latitude,
+        ]);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// ユーザー位置情報取得 API仕様書の114行目
+Route::get('/v1/user/location', 'Api\UserLocationController@index')->middleware('exsit.token');
+
+// ユーザー位置情報記録 API仕様書の142行目
+Route::post('/v1/user/location', 'Api\UserLocationController@create')->middleware('exsit.token');
+
+// ユーザー位置情報更新 API仕様書の177行目
+Route::patch('/v1/user/location', 'Api\UserLocationController@create')->middleware('exsit.token');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
