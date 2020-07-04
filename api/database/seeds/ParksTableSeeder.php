7a20b63f461eca6e2a0738288bf12228f36191f1
kazuma <deetbeet1234@yahoo.co.jp>
2020-07-04 18:09:10 +0900

    parkstabledatabaseを製作

--- api/database/seeds/ParksTableSeeder.php
+++ api/database/seeds/ParksTableSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-        ParksTableSeeder::class,
-        ]);
+      DB::table('parks')->insert([
+
+      ]);
     }
 }
