ed185c8722d26361503f79d0b4c977cfe2434d35
kazuma <deetbeet1234@yahoo.co.jp>
2020-07-06 00:43:22 +0900

    公園の情報が配列化しました。

--- api/database/seeds/ParksTableSeeder.php
+++ api/database/seeds/ParksTableSeeder.php
@@ -13,10 +13,14 @@ class DatabaseSeeder extends Seeder
     {
         $park_path = storage_path('park_data/P13-11_01_GML/KS-META-P13-11_01.xml');
         $word = file_get_contents($park_path);
-        $data = mb_convert_encoding($word, 'UTF-8', 'SJIS');
-        // $xml = simplexml_load_file($data);
-        $xml_str = str_replace('encoding="Shift_JIS"', 'encoding="UTF-8"', $data );
-        $parkdata = json_decode(json_encode(simplexml_load_string($xml_str)), true);
+        // $data = mb_convert_encoding($word, 'UTF-8', 'SJIS');
+        // $xml_str = str_replace('encoding="Shift_JIS"', 'encoding="UTF-8"', $data );
+        $xml = simplexml_load_string($word);
+        
+        $data = mb_convert_encoding($xml, 'UTF-8', 'SJIS');
+        $red = get_object_vars($xml);
+        
+        $parkdata = json_decode(json_encode($xml), true);
 
         \Log::debug($parkdata);
     //   DB::table('parks')->insert([
