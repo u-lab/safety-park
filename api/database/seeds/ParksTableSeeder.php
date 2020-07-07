bcaa4e82baf35fc284881f055f427ef845c3a13b
kazuma <deetbeet1234@yahoo.co.jp>
2020-07-07 22:28:31 +0900

    名前空間つきタグを扱うのが難しい

--- api/database/seeds/ParksTableSeeder.php
+++ api/database/seeds/ParksTableSeeder.php
@@ -11,18 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $park_path = storage_path('park_data/P13-11_01_GML/KS-META-P13-11_01.xml');
+        ini_set("memory_limit", "600M");
+
+        $park_path = storage_path('park_data/P13-11_01_GML/P13-11_01.xml');
         $word = file_get_contents($park_path);
         // $data = mb_convert_encoding($word, 'UTF-8', 'SJIS');
         // $xml_str = str_replace('encoding="Shift_JIS"', 'encoding="UTF-8"', $data );
+        // $xml = simplexml_load_string($word);
         $xml = simplexml_load_string($word);
+
+        $name_space = $xml->getNamespaces(true);
+
+        $g_node = $xml->children('gml', true)->Point;
         
-        $data = mb_convert_encoding($xml, 'UTF-8', 'SJIS');
-        $red = get_object_vars($xml);
+        // $data = mb_convert_encoding($xml, 'UTF-8', 'SJIS');
+        // $red = get_object_vars($xml);
         
         $parkdata = json_decode(json_encode($xml), true);
 
-        \Log::debug($parkdata);
+        // $value = $xml->identificationInfo->MD_DataIdentification->citation->title;
+
+        \Log::debug($g_node);
     //   DB::table('parks')->insert([
 
     //   ]);
