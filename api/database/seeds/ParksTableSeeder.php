9303dbdb349c19f110c5a36d9f69a155e3ff50fa
kazuma <deetbeet1234@yahoo.co.jp>
2020-07-08 23:55:31 +0900

    長いからテスト用のxml文書を準備した

--- api/database/seeds/ParksTableSeeder.php
+++ api/database/seeds/ParksTableSeeder.php
@@ -13,16 +13,16 @@ class DatabaseSeeder extends Seeder
     {
         ini_set("memory_limit", "600M");
 
-        $park_path = storage_path('park_data/P13-11_01_GML/P13-11_01.xml');
+        $park_path = storage_path('park_data/test.xml');
         $word = file_get_contents($park_path);
-        // $data = mb_convert_encoding($word, 'UTF-8', 'SJIS');
+        $data = mb_convert_encoding($word, 'UTF-8', 'SJIS');
         // $xml_str = str_replace('encoding="Shift_JIS"', 'encoding="UTF-8"', $data );
         // $xml = simplexml_load_string($word);
-        $xml = simplexml_load_string($word);
+        $xml = simplexml_load_string($word, 'SimpleXMLElement', LIBXML_NOCDATA);
 
         $name_space = $xml->getNamespaces(true);
 
-        $g_node = $xml->children('gml', true)->Point;
+        // $g_node = $xml->children('gml', true)->Point;
         
         // $data = mb_convert_encoding($xml, 'UTF-8', 'SJIS');
         // $red = get_object_vars($xml);
@@ -31,7 +31,10 @@ class DatabaseSeeder extends Seeder
 
         // $value = $xml->identificationInfo->MD_DataIdentification->citation->title;
 
-        \Log::debug($g_node);
+        $wd = $xml->xpath('/Dataset/ksj:Park/ksj:nop/text()');
+        $te = $wd[0];
+
+        \Log::debug($te);
     //   DB::table('parks')->insert([
 
     //   ]);
