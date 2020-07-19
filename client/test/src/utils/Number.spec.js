d5b31bce1f361a92de4a527ed374db13e014e5f4
H37kouya <h37kouya@gmail.com>
2020-07-19 11:17:58 +0900

    test テストの修正

---
+++ client/test/src/utils/Number.spec.js
@@ -0,0 +1,27 @@
+import { randInteger } from "~/src/utils/Number"
+
+describe('src/utils/Number', () => {
+  describe('randInteger', () => {
+
+    it('min = 0, max = 0のとき、0が返ってくるか', () => {
+      expect(randInteger(0, 0)).toBe(0)
+    })
+
+    it('min = 0, max = 3のとき、0 ~ 3すべて取得できるか', () => {
+      const result = [0, 0, 0, 0]
+      const error = 0
+
+      for (let i = 0; i < 1e6; i++) {
+        const rand = randInteger(0, 3)
+        if ([0, 1, 2, 3].includes(rand)) {
+          result[rand]++
+        } else {
+          error++
+        }
+      }
+
+      expect(result.every(num => num > 0)).toBeTruthy() // すべて 0 ~ 3の値は、1回は出現したか
+      expect(error).toBe(0)
+    })
+  })
+})
