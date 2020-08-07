930e7953c253f1f8f28fd46093a3f4ccbd8d495a
hideyukiaka <46902768+hideyukiaka@users.noreply.github.com>
2020-08-07 21:33:27 +0900

    geojsonデータの読みこみまでできた

--- client/components/Map.vue
+++ client/components/Map.vue
@@ -2,6 +2,12 @@
   <div class="map">
     <MglMap :accessToken='accessToken' :mapStyle='mapStyle' :center='center' :minZoom='minzoom'>
       <MglGeolocateControl position="bottom-right" />
+      <MglGeojsonLayer
+        :sourceId="geoJsonSource.features.id"
+        :source="geoJsonSource"
+        layerId="myLayer"
+        :layer="geoJsonlayer"
+      />
     </MglMap>
   </div>
 </template>
@@ -9,23 +15,36 @@
 <script>
 // Mapboxのラッパーライブラリをインポート
 import Mapbox from 'mapbox-gl'
-import { MglMap, MglGeolocateControl } from 'vue-mapbox'
+import { MglMap, MglGeolocateControl, MglGeojsonLayer } from 'vue-mapbox'
 
 export default {
   components: {
     MglMap,
-    MglGeolocateControl
+    MglGeolocateControl,
+    MglGeojsonLayer
   },
   data () {
     return {
       accessToken: 'pk.eyJ1Ijoic3luc2NoaXNtbyIsImEiOiJja2E5eHEwbXAweHdyMnlxcjlzMDVjMm56In0.lOPjbTfTjop6jTk58sOhTQ',
       mapStyle: 'mapbox://styles/synschismo/cka9xvauz00w31ilcr6ganv88',
       minzoom: 11,
-      center: [139.540667, 35.650614]
+      center: [139.540667, 35.650614],
+      geoJsonSource: null,
+      geoJsonLayer: null
     }
   },
-  created () {
+  async created () {
+    await this.fetchGeoJson()
     this.mapbox = Mapbox
+  },
+  methods: {
+    // GeoJsonの取得
+    async fetchGeoJson () {
+      // geojsonを取得
+      const geoJsonSource = await this.$axios.$get(`${this.$config.clientUrl}/geojson/tokyo.geojson`)
+      // vueインスタンス内のgeojsonを更新
+      this.geoJsonSource = geoJsonSource
+    }
   }
 }
 </script>
