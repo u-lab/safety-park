9dac3a8312180da91e1fef3435e35bf034008af0
kazuma <deetbeet1234@yahoo.co.jp>
2020-08-08 14:33:25 +0900

    Update Map.vue

--- client/components/Map.vue
+++ client/components/Map.vue
@@ -1,9 +1,52 @@
 <template>
-  <div></div>
+  <div class="map">
+    <MglMap :accessToken='accessToken' :mapStyle='mapStyle' :center='defaultCoordinates' :zoom='zoom'>
+      <MglGeolocateControl position="bottom-right" />
+      <MglGeojsonLayer
+        :sourceId="geoJsonSource.id"
+        :source="geoJsonSource"
+        layerId="myLayer"
+        :layer="geoJsonlayer"
+      />
+    </MglMap>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-div{
+<script>
+//Mapboxのラッパーライブラリをインポート
+import Mapbox from 'mapbox-gl'
+import { MglMap, MglGeolocateControl, MglGeojsonLayer } from 'vue-mapbox'
+
+export default {
+  components: {
+    MglMap,
+    MglGeolocateControl,
+    MglGeojsonLayer
+  },
+  data () {
+    return {
+      accessToken: 'pk.eyJ1Ijoic3luc2NoaXNtbyIsImEiOiJja2E5eHEwbXAweHdyMnlxcjlzMDVjMm56In0.lOPjbTfTjop6jTk58sOhTQ',
+      mapStyle: 'mapbox://styles/synschismo/cka9xvauz00w31ilcr6ganv88',
+      zoom: 11,
+      defaultCoordinates: [139.540667, 35.650614],
+      coordinates: [139.540667, 35.650614],
+      geoJsonSource: {
+        // GeoJSON をURLから読み込む方法がわからない
+      },
+      geoJsonLayer: {
+        //...some GeoJSON layer configuration object
+      }
+    }
+  },
+  created () {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.map {
   z-index: 9;
   height: 408px; /*50.24630542vh*/
   width: 89vw;
