12c8ada3b0c363d221bdfb1a597b07bc9b095120
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-08-09 23:53:57 +0900

    wip feat graphcontroller一時コミット

--- api/app/Http/Controllers/Api/GraphController.php
+++ api/app/Http/Controllers/Api/GraphController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class GraphController extends Controller
 {
     public function show(Request $request){
+        $yesterday = new Carbon('yesterday'); //前日を取得
+        $user_location = UserLocation::whereDay('start_time','=', $yesterday)->where('park_id', '=', $request->park_id)->all();
+        $hours=[];//return用の配列を初期化
+        \Log::debug($user_location);
         
+        return $hours;
+
     }
 }
