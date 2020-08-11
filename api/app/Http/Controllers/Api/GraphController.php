68daaf73252731735204d70abd245f12b4bea57d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2020-08-11 21:39:36 +0900

    fix 昨日の取得で先月の同じ日を取得してしまうミスを修正

--- api/app/Http/Controllers/Api/GraphController.php
+++ api/app/Http/Controllers/Api/GraphController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserLocation;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+
 
 class GraphController extends Controller
 {
     public function show(Request $request){
-        $yesterday = new Carbon('yesterday'); //前日を取得
-        $user_location = UserLocation::whereDay('start_time','=', $yesterday)->where('park_id', '=', $request->park_id)->all();
+        $yesterday = Carbon::yesterday();; //前日を取得
+        \Log::debug($yesterday);
+        $user_location = UserLocation::whereDate('start_time','=', $yesterday)->where('park_id', '=', $request->id)->get();//requestから送られてくるidがpark_id
         $hours=[];//return用の配列を初期化
         \Log::debug($user_location);
         
